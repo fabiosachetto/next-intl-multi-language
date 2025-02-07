f1e11f5cf2cf807ffe51aeb65a829a9d0836fbae
fabiosachetto <fabio@sachetto.com.br>
2025-02-06 23:36:48 -0300

    subindo tradução já funcionando ao clicar no select e arrumando erro do deploy

--- my-app/src/app/[locale]/layout.tsx
+++ my-app/src/app/[locale]/layout.tsx
@@ -30,7 +30,7 @@ export default async function LocaleLayout({
   children: React.ReactNode;
   params: {locale: string};
 }) {
-  const {locale} = params;
+  const {locale} = await params;
   if (!routing.locales.includes(locale as Locale)) {
     notFound();
   }
