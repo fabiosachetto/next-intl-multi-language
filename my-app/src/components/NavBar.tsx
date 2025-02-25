63a5fdf6ba5bde188e0fa7d20930ce73448df419
verdi <fabio@sachetto.com.br>
2025-02-24 22:33:09 -0300

    subindo pra tratar erro no vercel de novo

--- my-app/src/components/NavBar.tsx
+++ my-app/src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import LocaleSwitcher from "./LocaleSwitcher";
 
 export default function NavBar() {
   return (
-    <div className="flex justify-between items-center p-4">
+    <div className="flex justify-between items-center p-4 float-right">
       <LocaleSwitcher />
     </div>
   );
